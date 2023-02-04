70ddea1133dcb033dd81e3689f91cff663d2635c
OlegVasil <100850508+OlegVasil@users.noreply.github.com>
2023-02-04 12:19:22 +0300

    Update Laba1_1.rb

--- laba_1/Laba1_1.rb
+++ laba_1/Laba1_1.rb
@@ -2,11 +2,11 @@
 puts "Hello World"
 
 ####_1.2._####
-puts "Здравствуйте! Как к Вам обращаться?"
-name = gets.chomp
-puts "Очень приятно," + name
-puts "Введите любимый язык (программирования)"
-language = gets.chomp
+input_name = ARGV[0]
+puts "Очень приятно #{input_name}"
+puts "#{input_name}, введите любимый язык (программирования)"
+name = input_name
+language = STDIN.gets.chomp
 case language
 when "Ruby"
   puts "Что пользователь подлиза"
@@ -20,9 +20,9 @@ end
 
 ####_1.3._###
 puts name+", введите команду на Ruby"
-r_command = gets
-system "ruby -e \"#{r_command}\"" #ВАЖНО. Экранируем кавычки. Пример: puts \"Hello World\"
+r_command = STDIN.gets
+system "ruby -e \"#{r_command}\""
 
 puts name+", введите команду ОС" #ls
-oc_command = gets.chomp
+oc_command = STDIN.gets.chomp
 system (oc_command)
