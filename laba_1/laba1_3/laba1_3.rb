f73c14769fc77ddf6cdc3c58d9018c9328ca5640
OlegVasil <100850508+OlegVasil@users.noreply.github.com>
2023-02-11 20:45:36 +0300

    Добавил менюшки

--- laba_1/laba1_3/laba1_3.rb
+++ laba_1/laba1_3/laba1_3.rb
@@ -1,42 +1,68 @@
-def find_min(array)
-  return nil if array.empty?
-  min = array[0]
-  for element in array
-    min = element if element<min
-  end
-  min
-end
-
-def find_positive(array)
-  return nil if array.empty?
-  for index in 0...array.length
-    if array[index]>0
-      return index
+def min_elem_index(array) #2 - Индекс минимального элемента
+  min_element = array[0]
+  min_index = 0
+  array.each_with_index do |element, index|
+    if element < min_element
+      min_element = element
+      min_index = index
     end
   end
-  return -1
+  min_index
 end
 
-path = ARGV[0]
-task=ARGV[1]
-
-file = File.open(path)
-array = file.readline.split(' ').map(&:to_i)
-
-def menu(task,array)
-  puts("Ваш массив: #{array}")
-  puts("Задачи: \n 1.Найти минимальный элемент \n 2.Найти первый положительный элемент")
-  case task
-  when "1"
-    puts("Вы выбрали задачу: 1")
-    puts("Минимальный элемент: #{find_min(array)}")
-  when "2"
-    puts("Вы выбрали задачу: 2")
-    puts("Индекс первого положительного элемента: #{find_positive(array)}")
+def menu_list #Менюшка для выбора задачи
+  puts()
+  puts("Выберите задачу: ")
+  puts("1.Индекс минимального элемента")
+  puts("2.Количество элементов на интервале a...b")
+  puts("3.Количество элеменов между первым и последним минимальным")
+  puts("4.Количество элементов, значение которых принадлежит отрезку a...b")
+  puts("5.Неповторяющиеся элементы списков.")
+  answer = gets.chomp.to_i
+  if answer>0 && answer<6
+    answer
   else
-    puts"Некорректный ввод"
+    puts("Неккоректный ввод. \nВведите заново.")
+    menu_list
   end
 end
 
-menu(task,array)
-#ruby laba1_3.rb ./array.txt 1 вызывается из папки с файлом ( . )
+def input_choice #Менюшка для выбора способа ввода
+  puts()
+  puts "Вам файл или клавиатуру?"
+  answer = gets.chomp.to_i
+  case answer
+  when 1
+    puts "Вы выбрали файлы. \nУчтите, файл должен лежать в директории с программой. \nВведите название"
+    input_file = "./ #{gets.chomp}"
+    File.open(input_file)
+    array = input_file.readline.split(' ').map(&:to_i)
+  when 2
+    puts "Вы выбрали ручной ввод. \nВведите ваши данные через пробел"
+    array = gets.chomp.split.map(&:to_i)
+  else
+    puts "Неккоректный ввод. \nВведите заново."
+    input_choice
+  end
+  array
+end
+
+task = menu_list
+array = input_choice
+
+def results(task,array) #Вывод резульатов 
+  case task
+  when 1
+    puts ("Индекс минимального элемента: #{min_elem_index(array)}")
+  when 2
+    puts("Еще не готово")
+  when 3
+    puts("Еще не готово")
+  when 4
+    puts("Еще не готово")
+  when 5
+    puts("Еще не готово")
+  end
+end
+
+results(task,array)
