4c35e0d2ed3995e585cbdd8efe2e339737f7b1e6
OlegVasil <100850508+OlegVasil@users.noreply.github.com>
2023-03-03 22:38:21 +0300

    Чтоб гит без @ был

--- laba_2/laba_2_1/test.rb
+++ laba_2/laba_2_1/test.rb
@@ -2,7 +2,7 @@ require_relative 'student'
 student = Student.new("Кирилл", "Душнилин", "Кириллович")
 student.id = 2
 student.phone = "88005553535"
-student.telegram = "@kirill"
+student.telegram = "kirill"
 student.email = "kirill@mail.com"
 student.git = "kirilll"
 
