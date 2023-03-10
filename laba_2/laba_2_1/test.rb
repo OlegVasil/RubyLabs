40cfccf2bd6fde4129affe8961815daf36e197ee
OlegVasil <100850508+OlegVasil@users.noreply.github.com>
2023-03-10 21:02:53 +0300

    some new tests

--- laba_2/laba_2_1/test.rb
+++ laba_2/laba_2_1/test.rb
@@ -1,12 +1,15 @@
 require_relative 'student'
-student = Student.new("Кирилл", "Душнилин", "Кириллович")
+student = Student.new("Душнилин", "Кирилл", "Кириллович")
 student.id = "2"
 student.phone = "88005553535"
 student.telegram = "kirill"
 student.mail = "kirill@mail.com"
+
+jorik = Student.new("Воронин", "Георгий", "Отчество",   {git: 'git'})
+
 #student.git = "kirilll"
-
-
+#puts jorik
+#puts jorik.validate_contacts
 #puts(student)
 #puts Student.is_git?("git")
 #student.set_contacts({telegram: 'telega'})
