d721985156753c665b20ec0781b9ff9320d065dd
OlegVasil <100850508+OlegVasil@users.noreply.github.com>
2023-03-18 10:34:33 +0300

    test constuctor

--- laba_2/laba_2_1/test.rb
+++ laba_2/laba_2_1/test.rb
@@ -1,11 +1,11 @@
 require_relative 'student'
-student = Student.new("Душнилин", "Кирилл", "Кириллович")
-student.id = "2"
-student.phone = "88005553535"
-student.telegram = "kirill"
-student.mail = "kirill@mail.com"
+#student = Student.new("Душнилин", "Кирилл", "Кириллович")
+#student.id = "2"
+#student.phone = "88005553535"
+#student.telegram = "kirill"
+#student.mail = "kirill@mail.com"
 
-jorik = Student.new("Воронин", "Георгий", "Отчество",   {git: 'git'})
+#jorik = Student.new("Воронин", "Георгий", "Отчество",   {git: 'git'})
 
 #student.git = "kirilll"
 #puts jorik
@@ -14,3 +14,9 @@ jorik = Student.new("Воронин", "Георгий", "Отчество",   {g
 #puts Student.is_git?("git")
 #student.set_contacts({telegram: 'telega'})
 #puts student.telegram
+
+st1 = Student.new(last_name: "Vas", first_name: "V", surname: "C")
+puts st1
+
+st2=Student.from_json_str('{"last_name":"A", "first_name": "B", "surname": "C"}')
+puts st2
