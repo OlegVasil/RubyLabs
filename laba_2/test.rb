42b5d322f80863a48a67fe2e2e794424a78ec336
OlegVasil <100850508+OlegVasil@users.noreply.github.com>
2023-04-01 11:23:33 +0300

    read_from_txt

--- laba_2/test.rb
+++ laba_2/test.rb
@@ -15,8 +15,22 @@ require_relative 'student'
 #student.set_contacts({telegram: 'telega'})
 #puts student.telegram
 
-st1 = Student.new(last_name: "Vas", first_name: "V", surname: "C")
-puts st1
+#st1 = Student.new(last_name: "Vas", first_name: "V", surname: "C")
+#puts st1
+#
+#st2=Student.from_json_str('{"last_name":"A", "first_name": "B", "surname": "C"}')
+#puts st2
+require_relative 'student'
 
-st2=Student.from_json_str('{"last_name":"A", "first_name": "B", "surname": "C"}')
-puts st2
+def read_from_txt(file_path)
+  raise ArgumentError, 'Файл не найден' unless File.exist?(file_path)
+  file = File.open(file_path){|file| file.read}
+  JSON.parse(file).inject([]) do |list, student|
+    list << Student.from_json_str(student.to_json)
+  end
+end
+
+st_list=read_from_txt('/home/oleg/Рабочий стол/ПАТТЕРНЫ/laba_2/laba_2_1/st_i')
+st_list.each do |st|
+  puts st.get_info
+end
