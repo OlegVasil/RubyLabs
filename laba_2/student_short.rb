6e967fc75de7a1029df661f67a4a94abd1126638
OlegVasil <100850508+OlegVasil@users.noreply.github.com>
2023-04-01 09:14:33 +0300

    Убрал дублирование кода

--- laba_2/student_short.rb
+++ laba_2/student_short.rb
@@ -1,25 +1,35 @@
-class StudentShort
-  attr_reader :id, :git, :contact, :short_name
+require_relative 'base_student'
 
-  #стандартный конструктор из строки
+class StudentShort<BaseStudent
+
+  public_class_method :new
+
+  attr_reader :short_name
+  
+  private
+  attr_writer :short_name
+  
+  public
+
   def initialize(id, str)
     data = JSON.parse(str).transform_keys(&:to_sym)
-    raise ArgumentError, 'Fields required: short_name' if !data.key?(:short_name) || data[:short_name].nil?
-    @id = id
-    @short_name = data[:short_name]
-    @contact = data[:contact]
-    @git = data[:git]
+    self.short_name = data[:short_name]
+    super(id:id, git:data[:git], phone:data[:phone], mail: data[:mail], telegram: data[:telegram])
   end
 
   def self.init_from_student(student)
-    StudentShort.new(student.id, JSON.generate({"git":student.git, "short_name": student.short_name, "contact": student.find_contacts}))
+    StudentShort.new(student.id, JSON.generate({"short_name": student.short_name, "phone": student.phone, "telegram": student.telegram, "mail": student.mail}))
   end
 
   def to_s
     result = short_name
     result += " id=#{id} " unless id.nil?
-    result += " contact: #{contact}" unless contact.nil?
-    result+= " git: #{git}" unless git.nil?
+    result += " #{find_contacts}"
+    result+= " #{find_git}"
     result
+  end
+
+  def validate_all?
+    has_git? && has_contact?
   end
 end
