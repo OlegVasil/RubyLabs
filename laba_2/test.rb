b77ef794d379fe45859ba7dce792fa81b6edefe4
OlegVasil <100850508+OlegVasil@users.noreply.github.com>
2023-04-01 12:33:06 +0300

    read/write to txt

--- laba_2/test.rb
+++ laba_2/test.rb
@@ -1,26 +1,4 @@
 require_relative 'student'
-#student = Student.new("Душнилин", "Кирилл", "Кириллович")
-#student.id = "2"
-#student.phone = "88005553535"
-#student.telegram = "kirill"
-#student.mail = "kirill@mail.com"
-
-#jorik = Student.new("Воронин", "Георгий", "Отчество",   {git: 'git'})
-
-#student.git = "kirilll"
-#puts jorik
-#puts jorik.validate_contacts
-#puts(student)
-#puts Student.is_git?("git")
-#student.set_contacts({telegram: 'telega'})
-#puts student.telegram
-
-#st1 = Student.new(last_name: "Vas", first_name: "V", surname: "C")
-#puts st1
-#
-#st2=Student.from_json_str('{"last_name":"A", "first_name": "B", "surname": "C"}')
-#puts st2
-require_relative 'student'
 
 def read_from_txt(file_path)
   raise ArgumentError, 'Файл не найден' unless File.exist?(file_path)
@@ -34,3 +12,14 @@ st_list=read_from_txt('/home/oleg/Рабочий стол/ПАТТЕРНЫ/laba_
 st_list.each do |st|
   puts st.get_info
 end
+
+def write_to_txt(file_path, student_list)
+  res = '['
+  student_list.each do |st|
+    res += st.to_json_str + ","
+  end
+  res = res.chop + "]"
+  File.write(file_path, res)
+end
+
+write_to_txt('/home/oleg/Рабочий стол/ПАТТЕРНЫ/laba_2/laba_2_1/students_out.txt',st_list)
