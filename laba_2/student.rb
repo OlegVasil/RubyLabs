9ac40f5f841151225252ed9c2d538b5a1ba72dbf
OlegVasil <100850508+OlegVasil@users.noreply.github.com>
2023-04-01 09:15:15 +0300

    Убрал дублирование кода

--- laba_2/student.rb
+++ laba_2/student.rb
@@ -1,17 +1,17 @@
 require 'json'
+require_relative 'base_student'
+class Student<BaseStudent
+  public_class_method :new
 
-class Student
   #Краткое объявление метода
-  attr_accessor :id
-  attr_reader :last_name, :first_name, :surname, :phone, :telegram, :mail
+  attr_reader :last_name, :first_name, :surname
+  public :phone, :telegram, :mail, 'id=', 'phone=', 'mail=', 'telegram=', 'git='
 
   def initialize(last_name:, first_name:, surname:, id:nil, phone:nil, telegram:nil, mail:nil, git:nil)
     self.last_name = last_name
     self.first_name = first_name
     self.surname = surname
-    self.id = id
-    self.git = git
-    set_contacts(**{telegram:telegram, phone: phone, mail:mail})
+    super(id:id, phone:phone, telegram:telegram, mail:mail, git:git)
   end
 
   #Конструктор строки
@@ -21,28 +21,8 @@ class Student
   end
 
   #Валидация полей
-  def self.is_id?(id)
-    id.match(/^\d+$/)
-  end
-
   def self.is_name?(name)
     name.match(/^[А-Яа-я A-Za-z]+$/)
-  end
-
-  def self.is_telegram?(telegram)
-    telegram.match(/^[A-Za-z\d_-]+$/)
-  end
-
-  def self.is_mail?(mail)
-    mail.match(/^[^@\s]+@[^@\s]+\.[^@\s]+$/)
-  end
-
-  def self.is_git?(git)
-    git.match(/^[A-Za-z\d_-]+$/)
-  end
-
-  def self.is_phone?(phone)
-    phone.match(/^[\d{10}]+$/)
   end
 
   #Сеттеры полей
@@ -59,45 +39,6 @@ class Student
   def surname=(surname)
     raise ArgumentError, "Неккорректное отчество: #{surname}" unless surname.nil? || Student.is_name?(surname)
     @surname = surname
-  end
-
-  def phone=(phone)
-    raise ArgumentError, "Неккоректный номер: #{phone}" unless phone.nil? || Student.is_phone?(phone)
-    @phone = phone
-  end
-
-  def telegram=(telegram)
-    raise ArgumentError, "Неккоректный телеграм #{telegram}" unless telegram.nil? || Student.is_telegram?(telegram)
-      @telegram = telegram
-  end
-
-  def mail=(mail)
-    raise ArgumentError, "Неккоректный mail: #{mail}" unless mail.nil? || Student.is_mail?(mail)
-    @mail = mail
-  end
-
-  def git=(git)
-    raise ArgumentError, "Неккоректный гит: #{git}" unless git.nil? || Student.is_git?(git)
-    @git = git
-  end
-
-  #Валидация контактов с true или false
-  def has_git?
-    !git.nil?
-  end
-
-  def has_contact?
-    !phone.nil? || !telegram.nil? || !mail.nil?
-  end
-
-  def validate_all?
-    has_git? && has_contact?
-  end
-
-  def set_contacts(mail:nil, phone:nil, telegram:nil)
-    self.mail = mail
-    self.phone = phone
-    self.telegram = telegram
   end
 
   #Переопределил метод чтобы красиво выводилось
@@ -120,25 +61,6 @@ def find_git
   "git=#{git}" unless has_git?
 end
 
-def find_contacts
-  return "phone: #{phone}" unless phone.nil?
-  return "telegram: #{telegram}" unless telegram.nil?
-  "mail: #{mail}" unless mail.nil?
-end
-
 def get_info
   "#{short_name}, #{find_git}, #{find_contacts}"
 end
-
-#Валидация полей
-# def self.is_id?(id)
-#   id.match(/^\d+$/)
-# end
-# def self.from_str(str)
-#   hashed = str.split(',')
-#               .map{|v| v.split(":")}
-#               .map{|v| [v[0].to_sym, v[1]]}
-#               .to_h
-#   Student.new(hashed)
-#   #puts hashed
-# end
