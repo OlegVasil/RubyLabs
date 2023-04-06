8abec544077b94eebb8e7fb19e8b2ef91497087d
OlegVasil <100850508+OlegVasil@users.noreply.github.com>
2023-04-06 03:22:38 +0300

    Cтолбцы и строки

--- laba_2/data_table.rb
+++ laba_2/data_table.rb
@@ -1,7 +1,23 @@
 class DataTable
   def initialize(table)
-    self.table=table
+    self.table = table
+    self.row_number = table.length
+    self.column_number = count_column_number
   end
-  protected
+  attr_reader :row_number, :column_number
+
+  def get_item(row, col)
+    return nil if row>=row_number||col>=column_number
+    table[row][col].dup
+  end
+
+  private
   attr_accessor :table
+  attr_writer :row_number, :column_number
+
+  def count_column_number
+    columns_size=0
+    table.each{|el| columns_size=el.length if el.length>columns_size }
+    columns_size
+  end
 end
