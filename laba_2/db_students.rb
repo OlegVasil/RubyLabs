7bfc8bc68ea2a211ecf9b070781f71bc706183bd
OlegVasil <100850508+OlegVasil@users.noreply.github.com>
2023-05-20 10:23:29 +0300

    Реализован класс для работы с БД и одиночка

---
+++ laba_2/db_students.rb
@@ -0,0 +1,25 @@
+require 'sqlite3'
+class StudentDB
+  @@instance = nil
+  private def initialize
+    @db = SQLite3::Database.open 'db_student.sql'
+    @db.results_as_hash=true
+  end
+
+  def self.instance
+    if @@instance.nil?
+      @@instance=StudentDB.new
+    end
+    @@instance
+  end
+
+  def execute(query, *args)
+    @db.execute(query,*args)
+  end
+
+  def results_as_hash=(bool)
+    @db.results_as_hash=bool
+  end
+
+end# frozen_string_literal: true
+
